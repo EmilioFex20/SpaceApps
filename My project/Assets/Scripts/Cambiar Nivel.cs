a4bd3777cec45021cca0724e6c564fe5cd5ce3a5
Jessebnda <jesse.banda@cetys.edu.mx>
2023-10-07 17:39:56 -0700

    Boton con cambio de escena

---
+++ My project/Assets/Scripts/Cambiar Nivel.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Corrección de la siguiente línea
+using UnityEngine.SceneManagement;
+
+public class Cambiarnivel : MonoBehaviour
+{
+    public bool pasarNivel;
+    public int indiceNivel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debe ser "Input.GetKeyDown" en lugar de "Input.GetDown"
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CambiarNivel(indiceNivel);
+        }
+        if (pasarNivel)
+        {
+            // Debe tener un punto y coma al final de la línea
+            CambiarNivel(indiceNivel);
+        }
+    }
+
+    public void CambiarNivel(int indice)
+    {
+        SceneManager.LoadScene(indice);
+    }
+}
