36c8d02cfe5663d572b0e3dc81f9f584cd70ed55
Erick Blanco Nakashima <144400427+Nakashima26@users.noreply.github.com>
2023-10-08 21:11:51 -0700

    Update GameManager.cs

--- My project/Assets/Scripts/GameManager.cs
+++ My project/Assets/Scripts/GameManager.cs
@@ -82,7 +82,7 @@ public class GameManager : MonoBehaviour
     {
         if (SceneManager.GetActiveScene().buildIndex == 5)
         {
-            SceneManager.LoadScene(0);
+            SceneManager.LoadScene(1);
             currentLives = currentLives - 1;
             Debug.Log(currentLives);
         }
